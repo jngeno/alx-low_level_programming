61b8a6d20099cb4d1e0259f2bf95e7a1394861d3
jngeno <johnngeno.jn@gmail.com>
2022-10-19 22:27:24 +0300

    Absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - computes absolute value of an interger
+ * @t: The interger to be computed
+ * Return: the absolute value
+ */
+int_abs(int t)
+{
+	if (t >= 0)
+		return (t);
+	else
+		return (-t);
+}
